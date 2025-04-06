8879d781705bbe236199be2645bbc0cdfb8c0a15
Aleksey525 <alex_tolchin@mail.ru>
2025-04-06 12:00:21 +0300

    Update templates

--- barter_platform/settings.py
+++ barter_platform/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ads',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
