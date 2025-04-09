e7d393f7d96545d3ab85a32e7abd99468c83d234
Aleksey525 <alex_tolchin@mail.ru>
2025-04-09 08:48:50 +0300

    Add conftest

--- ads/tests/test_views.py
+++ ads/tests/test_views.py
@@ -1,36 +1,8 @@
-from django.contrib.auth.models import User
 from django.urls import reverse
 
 import pytest
 
 from ads.models import Ad, ExchangeProposal
-
-
-@pytest.fixture
-def user_sender(db):
-    """Создание пользователя-отправителя для тестов."""
-    return User.objects.create_user(username='sender_user', password='senderpass')
-
-@pytest.fixture
-def user_receiver(db):
-    """Создание пользователя-получателя для тестов."""
-    return User.objects.create_user(username='receiver_user', password='receiverpass')
-
-@pytest.fixture
-def ad_sender(db, user_sender):
-    """Создание объявления отправителя."""
-    return Ad.objects.create(title='Sender Ad', description='Sender Ad Description', user=user_sender)
-
-@pytest.fixture
-def ad_receiver(db, user_receiver):
-    """Создание объявления получателя."""
-    return Ad.objects.create(title='Receiver Ad', description='Receiver Ad Description', user=user_receiver)
-
-@pytest.fixture
-def client_logged_in(client, user_sender):
-    """Логиним клиента как отправителя для использования в тестах."""
-    client.login(username='sender_user', password='senderpass')
-    return client
 
 
 @pytest.mark.django_db
